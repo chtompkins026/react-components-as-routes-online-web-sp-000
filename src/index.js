bfc0e64b15796f1a7f6f7c21d77a450b9d3ca39b
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-22 16:20:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/* Add NavLink to importer */
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+ 
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+ 
+/* add the navbar component */
 const Navbar = () =>
   <div>
     <NavLink
